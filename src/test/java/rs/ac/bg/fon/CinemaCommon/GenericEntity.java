66af98014032d90633c47962a3e8b614fd5bf843
nikolaabadic <nikolaabadic@gmail.com>
2021-06-10 21:51:15 +0200

    GenericEntity interface added

---
+++ src/test/java/rs/ac/bg/fon/CinemaCommon/GenericEntity.java
@@ -0,0 +1,35 @@
+package rs.ac.bg.fon.CinemaCommon;
+
+import java.io.Serializable;
+import java.sql.ResultSet;
+import java.util.List;
+
+public interface GenericEntity extends Serializable{
+    String getTableName();
+
+    String getColumnNamesForInsert();
+    
+    String getDeleteString();
+    
+    String getUpdateString();    
+        
+    String getUpdateValues(GenericEntity entity);
+
+    String getInsertValues();
+    
+    String getInsertValues(int id);
+    
+    String getAlias();
+    
+    String getForeignKey();
+    
+    String getSecondForeignKey();
+    
+    String getPrimaryKey();
+
+    void setId(int id);
+    
+    List<GenericEntity> getList();
+        
+    List<GenericEntity> readList(ResultSet rs) throws Exception;
+}
