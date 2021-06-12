51f4e8f9da16bf3906e161c7c954db72cad1c453
nikolaabadic <nikolaabadic@gmail.com>
2021-06-12 20:09:35 +0200

    Admin class documented.

--- src/main/java/rs/ac/bg/fon/CinemaCommon/domain/Admin.java
+++ src/main/java/rs/ac/bg/fon/CinemaCommon/domain/Admin.java
@@ -5,16 +5,47 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Admin class.
+ * @author Nikola Abadic
+ *
+ */
 public class Admin implements GenericEntity {
+	/**
+	 * Administrator identification number.
+	 */
     private int adminID;
+    /**
+     * Administrator's first name.
+     */
     private String name;
+    /**
+     * Administrator's last name.
+     */
     private String surname;
+    /**
+     * Profile username.
+     */
     private String username;
+    /**
+     * Profile password.
+     */
     private String password;
 
+    /**
+     * Non-parameterized constructor used for initialization of Admin type objects.
+     */
     public Admin() {
     }
 
+    /**
+     * Parameterized constructor used for initialization of Admin type objects.
+     * @param adminID User identification number as an integer.
+     * @param name User's first name as a String.
+     * @param surname User's last name as a String.
+     * @param username Profile username as a String.
+     * @param password Profile password as a String.
+     */
     public Admin(int adminID, String name, String surname, String username, String password) {
         this.adminID = adminID;
         this.name = name;
@@ -22,47 +53,83 @@ public class Admin implements GenericEntity {
         this.username = username;
         this.password = password;
     }
-
+    
+    /**
+     * Returns profile password.
+     * @return Password as a String.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets profile password.
+     * @param password Password as a String.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /** 
+     * Returns administrator identification number.
+     * @return Administrator identification number as an integer.
+     */
     public int getAdminID() {
         return adminID;
     }
 
+    /**
+     * Sets the administrator identification number.
+     * @param adminID Administrator identification number as an integer.
+     */
     public void setAdminID(int adminID) {
         this.adminID = adminID;
     }
-
+    /**
+     * Returns administrator's first name.
+     * @return Administrator's first name as a String.
+     */
     public String getName() {
         return name;
     }
-
+    /** 
+     * Sets administrator's first name.
+     * @param name Administrator's first name as a String.
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Returns administrator's last name.
+     * @return Administrator's last name as a String.
+     */
     public String getSurname() {
         return surname;
     }
-
+    /**
+     * Sets administrator's last name.
+     * @param surname Administrator's last name as a String.
+     */
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
+    /**
+     * Returns profile username.
+     * @return Username as a String.
+     */
     public String getUsername() {
         return username;
     }
-
+    /**
+     * Sets the profile username.
+     * @param username Username as a String.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Returns profile username.
+     */
     @Override
     public String toString() {
         return username;
